1e87d33af7cc2d745cddcf3f0ea4042bfd86869d
Barbara P. Jaber <bpolianajaber@gmail.com>
2017-11-09 22:15:39 -0200

    Add questions

--- project/app/controllers/questions_controller.rb
+++ project/app/controllers/questions_controller.rb
@@ -1,6 +1,28 @@
 class QuestionsController < ApplicationController
-	def ex
-
+	
+	def index
+		@questions = Question.all.order('created_at DESC')
 	end	
+
+	def new
+
+	end
+
+	def create 
+		@question = Question.new(question_params)
+		@question.status = "Pendente"
+		@question.save
+
+		redirect_to @question 
+	end
+
+	def show 
+		@question = Question.find(params[:id])
+	end
+
+	private 
+		def question_params
+			params.require(:question).permit(:title,:body,:status)
+		end
 
 end

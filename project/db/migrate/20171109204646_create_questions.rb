b8b841d0463c6975d59a5e3e94a5210ef7ab59ed
Barbara P. Jaber <bpolianajaber@gmail.com>
2017-11-10 04:01:03 -0200

    Style and Structure

--- project/db/migrate/20171109204646_create_questions.rb
+++ project/db/migrate/20171109204646_create_questions.rb
@@ -3,7 +3,10 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
     create_table :questions do |t|
       t.string :title
       t.text :body
+      t.text :alternatives
       t.string :status
+      t.integer :id
+      t.integer :year 
       
       t.timestamps
     end
